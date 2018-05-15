9a23da1be7b5e0393b6428d2545c1b2dad3e9b55
ibmcndev <31728923+ibmcndev@users.noreply.github.com>
2018-05-15 22:42:41 +0100

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hi World!";
+   			dojo.query("span.shareSome-title")[0].textContent="Hi hi World!";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
